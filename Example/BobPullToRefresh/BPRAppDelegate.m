2540071e23d18b78ba3fc0c1d37b3fe2d2ce5750
bob <bobthekingofegypt@googlemail.com>
2014-10-12 18:13:55 +0100

    trying to get build to work properly

--- Example/BobPullToRefresh/BPRAppDelegate.m
+++ Example/BobPullToRefresh/BPRAppDelegate.m
@@ -12,8 +12,20 @@
 
 @implementation BPRAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+#ifdef COVERAGE
++ (void)initialize {
+    //need to flush test observer to get coverage details
+    [[NSUserDefaults standardUserDefaults] setValue:@"XCTestLog,GcovTestObserver"
+                                             forKey:@"XCTestObserverClass"];
+}
+#endif
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+#ifdef COVERAGE
+    return YES;
+    //During coverage we don't want to have to execute the below code as it has side effects
+#endif
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     BPRViewController *controller = [[BPRViewController alloc] init];
@@ -26,32 +38,13 @@
     [self.window makeKeyAndVisible];
     return YES;
 }
-							
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+#ifdef COVERAGE
+    extern void __gcov_flush(void);
+    __gcov_flush();
+#endif
 }
 
 @end
